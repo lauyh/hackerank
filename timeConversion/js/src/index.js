d34e1570411409f4db9a3f94ed45a6d0ee8d0008
Richard <32272387+lauyh@users.noreply.github.com>
2022-07-10 00:11:20 +0800

    simplify the solution

--- timeConversion/js/src/index.js
+++ timeConversion/js/src/index.js
@@ -2,19 +2,14 @@ function timeConversion(str) {
     const isNoon = str.slice(-2) === 'AM' ? false : true;
     const isTwelve = str.substring(0,2) === '12' ? true : false;
 
-    let hour = '';
-
     let tmp = str.replace(/AM|PM/, '').split(':')
+    let hour = tmp[0];
     let minute = tmp[1];
     let sec = tmp[2];
     if(!isNoon && isTwelve){
-        hour = '00'
-    }else if(isNoon && !isTwelve){
+        hour = '00';
+    }else if (isNoon && !isTwelve){
         hour = (parseInt(tmp[0]) + 12).toString()
-    }else if(isNoon && isTwelve){
-        hour = '12'
-    }else{
-        hour = tmp[0].toString();
     }
 
     return `${hour}:${minute}:${sec}`
